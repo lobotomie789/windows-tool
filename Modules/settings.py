ec3e8e2c4b5b5cfbb39da47a2db3610c6a0c55f6
lobotomie789 <featherine.py@gmail.com>
2026-03-03 20:18:26 +0100

    Update settings.py

--- Modules/settings.py
+++ Modules/settings.py
@@ -1,24 +1,29 @@
 import msvcrt, time, sys, json, os
 from colorama import Fore, init
 from pathlib import Path
-from help_option import is_selected, color
 
 init(autoreset=True)
 espace = ' '
-red = Fore.LIGHTRED_EX
-cyan = Fore.LIGHTCYAN_EX
+red = Fore.RED
+lightred = Fore.LIGHTRED_EX
+blue = Fore.BLUE
+lightblue = Fore.LIGHTBLUE_EX
+green = Fore.GREEN
+lightgreen = Fore.LIGHTGREEN_EX
+cyan = Fore.CYAN
+lightcyan = Fore.LIGHTCYAN_EX
+lightmagenta = Fore.LIGHTMAGENTA_EX
 yellow = Fore.LIGHTYELLOW_EX
-green = Fore.LIGHTGREEN_EX
 end = Fore.WHITE
 
 base_dir = Path(__file__).resolve().parent  # dossier Modules
 json_path = base_dir.parent/"Data"/"settings.json"
 
-all_values = {
+all_settings = {
     1: "return",
     2: "validate",
-    3: "color",
-    4: "placeholder",
+    3: "mainColor",
+    4: "otherColor",
     5: "placeholder",
     6: "placeholder",
     7: "placeholder",
@@ -26,7 +31,7 @@ all_values = {
     9: "placeholder",
 }
 
-classic_values = {
+key_values = {
     1: "ESC",
     2: "BACKSPACE",
     3: "TAB",
@@ -38,7 +43,7 @@ classic_values = {
     9: "G",
 }
 
-keys_code = {
+key_code = {
     "ESC": b'\x1b',
     "BACKSPACE": b'\x08',
     "TAB": b'\t',
@@ -48,6 +53,30 @@ keys_code = {
     "Q": b'q',
     "X": b'x',
     "G": b'g',
+}
+
+color_values = {
+    1: "RED",
+    2: "LIGHTRED",
+    3: "BLUE",
+    4: "LIGHTBLUE",
+    5: "GREEN",
+    6: "LIGHTGREEN",
+    7: "CYAN",
+    8: "LIGHTCYAN",
+    9: "LIGHTMAGENTA",
+}
+
+color_codes = {
+    "RED": red,
+    "LIGHTRED": lightred,
+    "BLUE": blue,
+    "LIGHTBLUE": lightblue,
+    "GREEN": green,
+    "LIGHTGREEN": lightgreen,
+    "CYAN": cyan,
+    "LIGHTCYAN": lightcyan,
+    "LIGHTMAGENTA": lightmagenta,
 }
 
 def clear_buffer():
@@ -70,146 +99,215 @@ def set_setting(key, value):
 def get_setting(key, default=None):
     settings = load_settings()
     return settings.get(key, default)
+
+MC = color_codes[get_setting("mainColor")]
+OC = color_codes[get_setting("otherColor")]
+
+def is_selected(number, selected):
+    global OC
+    if selected == number:
+        return f'{OC}--> '
+    else:
+        return '    '
+    
+def color(number, selected):
+    global OC
+    if selected == number:
+        return OC
+    else:
+        return ''
+
+def upArrow(var):
+    if var > 1 and var < 10:
+        var -= 1
+    elif var == 1:
+        var = 9
+    elif var > 10 and var < 19:
+        var -= 1
+    elif var == 10:
+        var = 18
+    return var
+        
+def downArrow(var):
+    if var > 0 and var < 9:
+        var += 1
+    elif var > 9 and var < 18:
+        var += 1
+    elif var == 18:
+        var = 10
+    elif var == 9:
+        var = 1
+    return var
     
 def manage_settings():
-    global selected
+    global selected, MC, OC
     selected = 1
     os.system('cls')
-    running = True
-    while running:
-        Continue = True
-        print()
-        print(f'{cyan}[?] Which parameter do you wish to edit the value of ?')
-        options = f'''
-{is_selected(1, selected)}{red}[{end}1{red}] {end}{color(1, selected)}Return back key {yellow}({get_setting("return")}){end}
-{is_selected(2, selected)}{red}[{end}2{red}] {end}{color(2, selected)}Validate choice key{end} {yellow}({get_setting("validate")}){end}
-{is_selected(3, selected)}{red}[{end}3{red}] {end}{color(3, selected)}Main program color{end} {yellow}({get_setting("color")}){end}
-{is_selected(4, selected)}{red}[{end}4{red}] {end}{color(4, selected)}Placeholder{end}
-{is_selected(5, selected)}{red}[{end}5{red}] {end}{color(5, selected)}Placeholder{end}
-{is_selected(6, selected)}{red}[{end}6{red}] {end}{color(6, selected)}Placeholder{end}
-{is_selected(7, selected)}{red}[{end}7{red}] {end}{color(7, selected)}Placeholder{end}
-{is_selected(8, selected)}{red}[{end}8{red}] {end}{color(8, selected)}Placeholder{end}
-{is_selected(9, selected)}{red}[{end}9{red}] {end}{color(9, selected)}Placeholder{end}
-'''
-        print(options)
-        print(f'{green}[+] Use your Arrow keys to navigate between options (UP / DOWN).')
-        print(f'{green}[+] Press ENTER to confirm your choice.')
-        print(f'{red}[!] Press ESC to go back.')
+    ring0 = True
+    
+    while ring0:
+        ring1 = True
+        print(f'''
+{lightcyan}[?] Which parameter do you wish to edit the value of ?{end}
+
+{is_selected(1, selected)}{MC}[{end}1{MC}] {end}{color(1, selected)}Return back{yellow} ({get_setting("return")}){end}
+{is_selected(2, selected)}{MC}[{end}2{MC}] {end}{color(2, selected)}Validate choice{end}{yellow} ({get_setting("validate")}){end}
+{is_selected(3, selected)}{MC}[{end}3{MC}] {end}{color(3, selected)}Main Color{end}{yellow} ({get_setting("mainColor")}){end}
+{is_selected(4, selected)}{MC}[{end}5{MC}] {end}{color(4, selected)}Others Colors{end}{yellow} ({get_setting("otherColor")}){end}
+{is_selected(5, selected)}{MC}[{end}6{MC}] {end}{color(5, selected)}Placeholder{end}{yellow} ({get_setting("placeholder")}){end}
+{is_selected(6, selected)}{MC}[{end}7{MC}] {end}{color(6, selected)}Placeholder{end}{yellow} ({get_setting("placeholder")}){end}
+{is_selected(7, selected)}{MC}[{end}8{MC}] {end}{color(7, selected)}Placeholder{end}{yellow} ({get_setting("placeholder")}){end}
+{is_selected(8, selected)}{MC}[{end}9{MC}] {end}{color(8, selected)}Placeholder{end}{yellow} ({get_setting("placeholder")}){end}
+{is_selected(9, selected)}{MC}[{end}9{MC}] {end}{color(9, selected)}Placeholder{end}{yellow} ({get_setting("placeholder")}){end}
+
+{OC}[+] Use your Arrow keys to navigate between options (UP / DOWN).
+{OC}[+] Press ENTER to confirm your choice.
+{MC}[!] Press ESC to go back.''')
         
-        while Continue:
+        while ring1:
+            MC = color_codes[get_setting("mainColor")]
+            OC = color_codes[get_setting("otherColor")]
             clear_buffer()
             key = msvcrt.getch()
             # Si touche spéciale (flèches, F1, etc.)
             if key in (b'\x00', b'\xe0'):
                 key = msvcrt.getch()  # On lit le 2e caractère
                 if key == b'H': #UP
-                    if selected > 1 and selected < 10:
-                        selected -= 1
-                    elif selected == 1:
-                        selected = 9
-                    elif selected > 10 and selected < 19:
-                        selected -= 1
-                    elif selected == 10:
-                        selected = 18
-                    Continue = False
+                    selected = upArrow(selected)
+                    ring1 = False
                 elif key == b'P': #DOWN
-                    if selected > 0 and selected < 9:
-                        selected += 1
-                    elif selected > 9 and selected < 18:
-                        selected += 1
-                    elif selected == 18:
-                        selected = 10
-                    elif selected == 9:
-                        selected = 1
-                    Continue = False
+                    selected = downArrow(selected)
+                    ring1 = False
             
             elif key == b'\x1b':  # ESC
-                running = False
-                Continue = False
+                ring0, ring1 = False, False
+                
             elif key == b'\r':  # ENTER
                 def CurrentValue(value):
-                    if get_setting(all_values[selected]) == value:
+                    if get_setting(all_settings[selected]) == value:
                         return "(Active)"
                     else:
                         return ''
+                    
                 def isTaken(value):
                     settings = load_settings().values()
-                    if value in settings and get_setting(all_values[selected]) != value:
+                    if value in settings and get_setting(all_settings[selected]) != value:
                         return "(Taken)"
                     else:
                         return ''
                     
                 os.system('cls')
-                running2 = True
+                ring2 = True
                 new_option = 1
-                while running2:
-                    Continue2 = True
-                    print()
-                    print(f'{cyan}[?] Option {selected} selected, please choose the new value.')
-                    print()
-                    print(f'Current {yellow}{all_values[selected]}{end} value : {green}{get_setting(all_values[selected])}')
-                    print(f'Available values :')
-                    options = f'''
-{is_selected(1, new_option)}{red}[{end}1{red}] {end}{color(1, new_option)}ESC{end} {yellow}{CurrentValue("ESC")}{red}{isTaken("ESC")}
-{is_selected(2, new_option)}{red}[{end}2{red}] {end}{color(2, new_option)}BACKSPACE{end} {yellow}{CurrentValue("BACKSPACE")}{red}{isTaken("BACKSPACE")}
-{is_selected(3, new_option)}{red}[{end}3{red}] {end}{color(3, new_option)}TAB{end} {yellow}{CurrentValue("TAB")}{red}{isTaken("TAB")}
-{is_selected(4, new_option)}{red}[{end}4{red}] {end}{color(4, new_option)}ENTER{end} {yellow}{CurrentValue("ENTER")}{red}{isTaken("ENTER")}
-{is_selected(5, new_option)}{red}[{end}5{red}] {end}{color(5, new_option)}R{end} {yellow}{CurrentValue("R")}{red}{isTaken("R")}
-{is_selected(6, new_option)}{red}[{end}6{red}] {end}{color(6, new_option)}T{end} {yellow}{CurrentValue("T")}{red}{isTaken("T")}
-{is_selected(7, new_option)}{red}[{end}7{red}] {end}{color(7, new_option)}Q{end} {yellow}{CurrentValue("Q")}{red}{isTaken("Q")}
-{is_selected(8, new_option)}{red}[{end}8{red}] {end}{color(8, new_option)}X{end} {yellow}{CurrentValue("X")}{red}{isTaken("X")}
-{is_selected(9, new_option)}{red}[{end}9{red}] {end}{color(9, new_option)}G{end} {yellow}{CurrentValue("G")}{red}{isTaken("G")}
-'''
-                    print(options)
-                    print()
-                    print(f'{green}[+] Use your Arrow keys to navigate between options (LEFT / RIGHT).')
-                    print(f'{green}[+] Press ENTER to confirm your choice.')
-                    print(f'{red}[!] Press ESC to go back.')
-                    while Continue2:
+                
+                while ring2:
+                    if selected != 3:
+                        ring3 = True
+                        print(f'''
+{lightcyan}[?] Option {selected} selected, please choose the new value.{end}
+
+Current {yellow}{all_settings[selected]}{end} value : {OC}{get_setting(all_settings[selected])}{end}
+Available values :
+
+{is_selected(1, new_option)}{MC}[{end}1{MC}] {end}{color(1, new_option)}ESC{end} {yellow}{CurrentValue("ESC")}{red}{isTaken("ESC")}
+{is_selected(2, new_option)}{MC}[{end}2{MC}] {end}{color(2, new_option)}BACKSPACE{end} {yellow}{CurrentValue("BACKSPACE")}{red}{isTaken("BACKSPACE")}
+{is_selected(3, new_option)}{MC}[{end}3{MC}] {end}{color(3, new_option)}TAB{end} {yellow}{CurrentValue("TAB")}{red}{isTaken("TAB")}
+{is_selected(4, new_option)}{MC}[{end}4{MC}] {end}{color(4, new_option)}ENTER{end} {yellow}{CurrentValue("ENTER")}{red}{isTaken("ENTER")}
+{is_selected(5, new_option)}{MC}[{end}5{MC}] {end}{color(5, new_option)}R{end} {yellow}{CurrentValue("R")}{red}{isTaken("R")}
+{is_selected(6, new_option)}{MC}[{end}6{MC}] {end}{color(6, new_option)}T{end} {yellow}{CurrentValue("T")}{red}{isTaken("T")}
+{is_selected(7, new_option)}{MC}[{end}7{MC}] {end}{color(7, new_option)}Q{end} {yellow}{CurrentValue("Q")}{red}{isTaken("Q")}
+{is_selected(8, new_option)}{MC}[{end}8{MC}] {end}{color(8, new_option)}X{end} {yellow}{CurrentValue("X")}{red}{isTaken("X")}
+{is_selected(9, new_option)}{MC}[{end}9{MC}] {end}{color(9, new_option)}G{end} {yellow}{CurrentValue("G")}{red}{isTaken("G")}
+
+{OC}[+] Use your Arrow keys to navigate between options (UP / DOWN).
+{OC}[+] Press ENTER to confirm your choice.
+{MC}[!] Press ESC to go back.''')
+
+                        while ring3:
+                            clear_buffer()
+                            key = msvcrt.getch()
+                            # Si touche spéciale (flèches, F1, etc.)
+                            if key in (b'\x00', b'\xe0'):
+                                key = msvcrt.getch()  # On lit le 2e caractère
+                                if key == b'H': #UP
+                                    new_option = upArrow(new_option)
+                                    ring3 = False
+                                elif key == b'P': #DOWN
+                                    new_option = downArrow(new_option)
+                                    ring3 = False
+
+                            elif key == b'\x1b':  # ESC
+                                ring1, ring2, ring3 = False, False, False
+
+                            elif key == b'\r':  # ENTER
+                                settings = load_settings().values()
+                                if key_values[new_option] in settings and get_setting(all_settings[selected]) == key_values[new_option]:
+                                    pass
+                                elif key_values[new_option] in settings and get_setting(all_settings[selected]) != key_values[new_option]:
+                                    pass
+                                else:
+                                    set_setting(all_settings[selected], key_values[new_option])
+                                    ring3 = False
+                                    MC = color_codes[get_setting("mainColor")]                  # FAIRE EN SORTE D'ACTUALISER LA COULEUR A CHAQUE CHANGEMENT DE COULEUR DU PANEL
+                                    OC = color_codes[get_setting("otherColor")]                 # FAIRE EN SORTE D'ACTUALISER LA COULEUR A CHAQUE CHANGEMENT DE COULEUR DU PANEL
+                                    os.system('cls')
+                                    clear_buffer()
+
+                        os.system('cls')
                         clear_buffer()
-                        key = msvcrt.getch()
-                        # Si touche spéciale (flèches, F1, etc.)
-                        if key in (b'\x00', b'\xe0'):
-                            key = msvcrt.getch()  # On lit le 2e caractère                       # IL RESTE A FAIRE EN SORTE DE NE PAS POUVOIR ECRIRE AUTRE CHOSE QUE CE QUI EST PREVU!!!
-                            if key == b'H': #UP
-                                if new_option > 1 and new_option < 10:
-                                    new_option -= 1
-                                elif new_option == 1:
-                                    new_option = 9
-                                elif new_option > 10 and new_option < 19:
-                                    new_option -= 1
-                                elif new_option == 10:
-                                    new_option = 18
-                                Continue2 = False
-                            elif key == b'P': #DOWN
-                                if new_option > 0 and new_option < 9:
-                                    new_option += 1
-                                elif new_option > 9 and new_option < 18:
-                                    new_option += 1
-                                elif new_option == 18:
-                                    new_option = 10
-                                elif new_option == 9:
-                                    new_option = 1
-                                Continue2 = False
+                        
+                    else: # Si c'est l'option de la couleur
+                        ring3 = True
+                        print(f'''
+{lightcyan}[?] Option {selected} selected, please choose the new value.{end}
 
-                        elif key == b'\x1b':  # ESC
-                            running2 = False
-                            Continue2 = False
-                            Continue = False
-                        elif key == b'\r':  # ENTER
-                            settings = load_settings().values()
-                            if classic_values[new_option] in settings and get_setting(all_values[selected]) == classic_values[new_option]:
-                                pass
-                            elif classic_values[new_option] in settings and get_setting(all_values[selected]) != classic_values[new_option]:
-                                pass
-                            else:
-                                set_setting(all_values[selected], classic_values[new_option])
-                                Continue2 = False
-                                os.system('cls')
-                                clear_buffer()
+Current {yellow}{all_settings[selected]}{end} value : {OC}{get_setting(all_settings[selected])}{end}
+Available values :
 
-                    os.system('cls')
-                    clear_buffer()
+{is_selected(1, new_option)}{MC}[{end}1{MC}] {end}{color(1, new_option)}RED{end} {yellow}{CurrentValue("RED")}{red}{isTaken("RED")}
+{is_selected(2, new_option)}{MC}[{end}2{MC}] {end}{color(2, new_option)}LIGHTRED{end} {yellow}{CurrentValue("LIGHTRED")}{red}{isTaken("LIGHTRED")}
+{is_selected(3, new_option)}{MC}[{end}3{MC}] {end}{color(3, new_option)}BLUE{end} {yellow}{CurrentValue("BLUE")}{red}{isTaken("BLUE")}
+{is_selected(4, new_option)}{MC}[{end}4{MC}] {end}{color(4, new_option)}LIGHTBLUE{end} {yellow}{CurrentValue("LIGHTBLUE")}{red}{isTaken("LIGHTBLUE")}
+{is_selected(5, new_option)}{MC}[{end}5{MC}] {end}{color(5, new_option)}GREEN{end} {yellow}{CurrentValue("OC")}{red}{isTaken("OC")}
+{is_selected(6, new_option)}{MC}[{end}6{MC}] {end}{color(6, new_option)}LIGHTGREEN{end} {yellow}{CurrentValue("LIGHTGREEN")}{red}{isTaken("LIGHTGREEN")}
+{is_selected(7, new_option)}{MC}[{end}7{MC}] {end}{color(7, new_option)}CYAN{end} {yellow}{CurrentValue("CYAN")}{red}{isTaken("CYAN")}
+{is_selected(8, new_option)}{MC}[{end}8{MC}] {end}{color(8, new_option)}LIGHTCYAN{end} {yellow}{CurrentValue("LIGHTCYAN")}{red}{isTaken("LIGHTCYAN")}
+{is_selected(9, new_option)}{MC}[{end}9{MC}] {end}{color(9, new_option)}LIGHTMAGENTA{end} {yellow}{CurrentValue("LIGHTMAGENTA")}{red}{isTaken("LIGHTMAGENTA")}
+
+{OC}[+] Use your Arrow keys to navigate between options (UP / DOWN).
+{OC}[+] Press ENTER to confirm your choice.
+{MC}[!] Press ESC to go back.''')
+
+                        while ring3:
+                            clear_buffer()
+                            key = msvcrt.getch()
+                            # Si touche spéciale (flèches, F1, etc.)
+                            if key in (b'\x00', b'\xe0'):
+                                key = msvcrt.getch()  # On lit le 2e caractère
+                                if key == b'H': #UP
+                                    new_option = upArrow(new_option)
+                                    ring3 = False
+                                elif key == b'P': #DOWN
+                                    new_option = downArrow(new_option)
+                                    ring3 = False
+
+                            elif key == b'\x1b':  # ESC
+                                ring1, ring2, ring3 = False, False, False
+
+                            elif key == b'\r':  # ENTER
+                                settings = load_settings().values()
+                                if key_values[new_option] in settings and get_setting(all_settings[selected]) == key_values[new_option]:
+                                    pass
+                                elif key_values[new_option] in settings and get_setting(all_settings[selected]) != key_values[new_option]:
+                                    pass
+                                else:
+                                    set_setting(all_settings[selected], color_values[new_option])
+                                    ring3 = False
+                                    os.system('cls')
+                                    clear_buffer()
+
+                        os.system('cls')
+                        clear_buffer()
 
         os.system('cls')
         clear_buffer()
